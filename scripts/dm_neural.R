823729a9feb80f0ba50e49ae57db33ae092d8b83
Peter Wasiewicz <pwasiewi@elka.pw.edu.pl>
2017-06-09 19:47:45 +0200

    new neural network sample

--- scripts/dm_neural.R
+++ scripts/dm_neural.R
@@ -9,6 +9,9 @@
 #https://www.r-bloggers.com/deep-learning-in-r-2/
 #http://www.deeplearningbook.org/contents/mlp.html
 #http://deeplearning.net/reading-list/
+#REINFORCEMENT LEARNING IN R
+#http://www.rblog.uni-freiburg.de/2017/02/26/reinforcement-learning-in-r/
+#http://www.rblog.uni-freiburg.de/2017/04/08/reinforcementlearning-a-package-for-replicating-human-behavior-in-r/
 #Included samples will run after installing needed packages R 
 #(in linux rather on root in the R console to avoid installing locally):
 
@@ -18,8 +21,9 @@ pkgcheck <- pkglist %in% row.names(installed.packages())
 pkglist[!pkgcheck]
 for(i in pkglist[!pkgcheck]){install.packages(i,depend=TRUE)}
 
+par(mfrow = c(3, 1)) #three pictures in one column
 #set the beginning of the random generator in order to obtain the same results at each run
-seed.val <- 1234567890
+seed.val <- 1234
 set.seed(seed.val)
 
 #the sigmoid threshold function
@@ -36,15 +40,15 @@ A1=c(1,0,0)
 y <- 0
 #two ways of 2D matrix generation
 #in this case the matrix of all connections between the input vector <X1, X2, b> and 3 hidden layer neurons
-nkolumn=3
-mwierszy=3 
-W1<-t(replicate(mwierszy, runif(nkolumn,-1,1)))
-W1<-matrix(runif(mwierszy*nkolumn), ncol=nkolumn)
+ncols=3
+mrows=3 
+W1<-t(replicate(mrows, runif(ncols,-1,1)))
+W1<-matrix(runif(mrows*ncols), ncol=ncols)
 
 #in this case the matrix of all connections between the hidden layer (the vector A2 plus b2) and the output neuron 
-nkolumn2=4
-mwierszy2=1 
-W2<-matrix(runif(mwierszy2*nkolumn2), ncol=nkolumn2)
+ncols2=4
+mrows2=1 
+W2<-matrix(runif(mrows2*ncols2), ncol=ncols2)
 
 #THE FIRST LOOP - FORWARD PROPAGATION OF A
 N2 <- W1 %*% A1
@@ -53,9 +57,8 @@ N3 <- c(W2 %*% A2)
 h <- A3 <- sigmoid(N3)
 h
 
-#THE NEXT LOOPS
-#ERROR BACKPROPAGATION
-alfa<-20
+#THE NEXT LOOP FOR BACKPROPAGATION IN ORDER TO change weights and minimize the error MSE
+alfa<-10
 J <- ((y * log(h)) + ((1 - y) * log(1 - h))) * -1
 delta3 = (h - y)*h*(1-h)
 #derivative of sigmoid(Z) is equal to sigmoid(Z)*(1-sigmoid(Z))
@@ -73,8 +76,10 @@ A2 <- c(1, sigmoid(N2))
 N3 <- c(W2 %*% A2)
 h <- A3 <- sigmoid(N3)
 h
+#END OF LOOP
 
 #set the beginning of the random generator in order to obtain the same results at each run
+seed.val <- 1234567890
 set.seed(seed.val)
 #########################################################################################################################
 # APPROXIMATION OF THE XOR FUNCTION ##############################################################################
@@ -89,8 +94,8 @@ xor_nn <-
            alpha  = 0.01) {
     # check whether weights needs initialization
     if (init_w == 1) {
-      W1 <- matrix(runif(mwierszy * nkolumn), ncol = nkolumn)
-      W2 <- matrix(runif(mwierszy2 * nkolumn2), ncol = nkolumn2)
+      W1 <- matrix(runif(mrows * ncols), ncol = ncols)
+      W2 <- matrix(runif(mrows2 * ncols2), ncol = ncols2)
     }
     # weight corrections from two layers: hidden and output ones
     T1_DELTA = array(0L, dim(W1))
@@ -100,7 +105,7 @@ xor_nn <-
     # cost function
     J <- 0.0
     #learned xor
-    wynik<-c()
+    result<-c()
     #disp('NN output ');
     for (i in 1:nrow(XOR)) {
       # signal forward propagation for output i=1
@@ -124,7 +129,7 @@ xor_nn <-
       else{
         cat('Hypothesis XOR for ', XOR[i, 1:2], 'equals ', h, '\n')
       }
-      wynik<-c(wynik,h)
+      result<-c(result,h)
     }
     J <- J / -m
     #cat('delta3: ', delta3, '\n')
@@ -137,7 +142,7 @@ xor_nn <-
     else{
       cat('J: ', J, '\n')
     }
-    list(W1,W2,wynik)
+    list(W1,W2,result)
   }
 
 #XOR function table for training: two first parameters are inputs, the third one is the output
@@ -165,21 +170,21 @@ for (i in 1:50000) {
   #execute with training and without initialization
   list[W1, W2,] <- xor_nn(XOR, W1, W2, 0, 1, 0.05)
   if (i %% 1000 == 0) {
-    cat('Iteracja : ', i, '\n')
-    #execute without initialization and training, just an answer of the  trained neural network
+    cat('Iteration : ', i, '\n')
+    #execute without initialization and training, just an answer of the trained neural network
     list[W1, W2,nauczone_xor] <- xor_nn(XOR, W1, W2)
   }
 }
-#results should be the same<;
+#results should be the same
 for (i in 1:nrow(XOR)) {
-  cat('Wartość XOR dla ', XOR[i, 1:2], 'wynosi ', XOR[i, 3], '\n')
+  cat('The XOR output for ', XOR[i, 1:2], 'equals ', XOR[i, 3], '\n')
 }
 (XOR[, 3] - nauczone_xor) ^ 2                #square differences between the training set output and the net result 
 sum((XOR[, 3] - nauczone_xor) ^ 2)           #Sum Squared Error
 #the final net error  
-pierwkwadsumkwadrozn <- sqrt(sum((XOR[, 3] - nauczone_xor) ^ 2))
-cat('The XOR function training net error', pierwkwadsumkwadrozn,'\n')
-Sys.sleep(2)                                 # pause 2 seconds
+rootofsse <- sqrt(sum((XOR[, 3] - nauczone_xor) ^ 2))
+cat('The XOR function training net error', rootofsse,'\n')
+Sys.sleep(2)                                 # pause for 2 seconds
 
 
 #########################################################################################################################
@@ -219,47 +224,48 @@ cbind(XOR[, 3])
 (cbind(XOR[, 3]) - nauczone_xor) ^ 2         #squared errors of prediction
 sum((cbind(XOR[, 3]) - nauczone_xor) ^ 2)    #Sum squared roots
 #the final net error  
-pierwkwadsumkwadrozn <- sqrt(sum((cbind(XOR[, 3]) - nauczone_xor) ^ 2))
-cat('The XOR function training nnet error', pierwkwadsumkwadrozn,'\n')
-Sys.sleep(7)                                 # pauza na 7 sekund
+rootofsse <- sqrt(sum((cbind(XOR[, 3]) - nauczone_xor) ^ 2))
+cat('The XOR function training nnet error', rootofsse,'\n')
+Sys.sleep(7)                                 # pause for 7 seconds
 
 
 #########################################################################################################################
-# APROKSYMACJA FUNKCJI SINUS W 20 PUNKTACH ##############################################################################
+# APPROXIMATION OF SINUS IN 20 POINTS      ##############################################################################
 #########################################################################################################################
-#R sieć neuronowa z pakietu nnet - nauka funkcji sinus w 20 punktach i aproksymacja reszty zakresu
+#R neural networks from a package nnet - training sinus function inputs and outputs in 20 points 
+#function will approximate the rest of the point domain
 library(clusterGeneration)
 library(corrplot)
-#importuj funkcję wizualizacji sieci neuronowej z Githuba
+#import net virtualization functions from github
 library(devtools)
 source_url(
   'https://gist.github.com/fawda123/7471137/raw/cd6e6a0b0bdb4e065c597e52165e5ac887f5fe95/nnet_plot_update.r'
 )
-#biblioteka nnet
+#library nnet
 library(nnet)
 
-#ustawienie początkowego stanu generatora losowego, aby wyniki za każdym razem były te same
+#set the beginning of the random generator in order to obtain the same results at each run
 seed.val <- 86644
 set.seed(seed.val)
 
-#ilość punktów z x do nauki
+#the number of points to train
 num.obs <- 20
-#ilość neuronów w pierwszej i jedynej warstwie sieci
+#numbers of neurons in the first (and the last) layer
 max.neurons <- 200
 
-#do nauki sieci 20 punktów co jeden
+#20 points to train
 x1 <- seq(1, num.obs, 1)
-#gęstsze próbkowanie do sprawdzenia działania sieci, jej aproksymacji między punktami uczenia
+#more dense probe for approximation checking
 xx1 <- seq(1, num.obs, 0.3)
 
-#dane do nauki, na końcowym wykresie czerwone punkty
+#data to train, at the picture red dots
 y1 <- sin(x1)
-#tak powinna działać sieć aproksymować ten wykres yy1, na końcowym wykresie zielona ciągła linia
+#the real function of sinus, at the final picture the green line
 yy1 <- sin(xx1)
 plot(x1, y1, col = "red")
 lines(xx1, yy1, col = "green")
 
-#dane pakowane w ramki danych specjalnie dla funkcji sieci neuronowej: X1 - wejście,  Y1 - wyjście do nauki
+#pack data into dataframes for nnet library: X1 - input,  Y1 - output to train
 rand.vars <- data.frame(x1)
 names(rand.vars) <- c('X1')
 resp <- data.frame(y1)
@@ -267,9 +273,9 @@ names(resp) <- c('Y1')
 dat.in <- data.frame(resp, rand.vars)
 dat.in
 
-#ustawienie początkowego stanu generatora losowego, aby wyniki za każdym razem były te same
+#set the beginning of the random generator in order to obtain the same results at each run
 set.seed(seed.val)
-#nauka sieci neuronowej z 20 neuronami i liniowym wyjściem z neurona
+#training of the neural network with the 20 neuron layer and linear output
 mod1 <- nnet(rand.vars,
              resp,
              data = dat.in,
@@ -277,33 +283,33 @@ mod1 <- nnet(rand.vars,
              linout = T)
 
 par(mfrow = c(3, 1))
-#pokaż nauczoną sieć, szare to minusowe, czarne to dodatnie wagi połączeń
+#show the net, gray are weights less than 0 and black are positive weights
 plot.nnet(mod1)
 
 
-#sprawdzenie działania sieci na gęstszej próbce xx1
+#checking the computed network on the more dense probe xx1
 x1
 xx1
 ypred <- predict(mod1, cbind(xx1))
 plot(xx1, ypred)
-#kwadrat różnic między założonymi yy1, a uzyskanymi wynikami ypred
-kwadroznicy <- (yy1 - ypred) ^ 2
-#suma kwadratów różnic
-sumkwadrozn <- sum((yy1 - ypred) ^ 2)
-#pierwiastek z sumy - końcowy błąd sieci neuronowej
-error1 <- sqrt(sumkwadrozn)
+#square differences between the training set output yy1 and the net result ypred
+squarederror <- (yy1 - ypred) ^ 2
+#sum squared error
+sumsquarederror <- sum((yy1 - ypred) ^ 2)
+#the final net error  
+error1 <- sqrt(sumsquarederror)
 error1
-Sys.sleep(2)                                 # pauza na 2 sekund
+Sys.sleep(2)                                 # pause for 2 seconds
 
 
 #########################################################################################################################
-#R sieć neuronowa nnet - badanie wpływu ilości neuronów warstwy ukrytej
-errorlist <- list()                          #pusta lista
-#przeprowadź naukę sieci od 4 do max.neurons np. 100 neuronów w jedynej warstwie
+#R neural networks from library nnet - influence of hidden layer neuron numbers
+errorlist <- list()                          #empty list
+#training networks from 4 to max.neurons e.g. 100 neurons in the only layer
 for (i in 4:max.neurons) {
-  #ustawienie początkowego stanu generatora losowego, aby wyniki za każdym razem były te same
+  #set the beginning of the random generator in order to obtain the same results at each run
   set.seed(seed.val)
-  #nauka sieci neuronowej z i (z pętli for) neuronami i liniowym wyjściem z neurona
+  #training of the neural network with the i (from the loop for) neuron layer and linear output
   mod1 <- nnet(
     rand.vars,
     resp,
@@ -312,31 +318,31 @@ for (i in 4:max.neurons) {
     linout = T,
     trace = FALSE
   )
-  #sprawdzenie działania sieci na gęstszej próbce xx1
+  #checking the computed network on the more dense probe xx1
   ypred <- predict(mod1, cbind(xx1))
-  #policzenie błędu z pierwiastka sumy kwadratów różnic
+  #the final error from rooted sum of squared errors
   error <- sqrt(sum((yy1 - ypred) ^ 2))
-  # i dodanie do listy w której indeks+3 oznacza liczbę neuronów w sieci
+  # and adding to the list which indeks+3 means the neuron number
   errorlist <- c(errorlist, error)
 }
-#przetworzenie listy do wektora
+#converting list to vector
 errorvector <- rapply(errorlist, c)
-#wyrysowanie wektora na wykresie - nie widać tendencji malejących lub rosnących
+#plotting of the error vector - there is no rising or other trends 
 plot(errorvector)
-#minimalny błąd
+#minimal error
 minerror <- min(errorvector)
 minerror
 #optimise<-which(errorvector %in% c(min(errorvector)))
-#i jego indeks czyli liczba neuronów zmniejszona o 3 gdyż pętla for zaczynała od liczby neuronów 4
+#and its indeks which started with a position no 1 for 4 neurons
 optimsize <- match(min(errorvector), errorvector)
 optimsize
-Sys.sleep(2)                                 # pauza na 2 sekund
+Sys.sleep(2)                                 # pause for 2 seconds
 
 #########################################################################################################################
-#nnet z optymalną liczbą neuronów
-#ustawienie początkowego stanu generatora losowego, aby wyniki za każdym razem były te same
+#nnet with the optimal neuron number
+#set the beginning of the random generator in order to obtain the same results at each run
 set.seed(seed.val)
-#ponowna nauka sieci z idealną liczbą neuronów (dającą najmniejszy błąd)
+#again training with the optimal neuron number for the smallest error
 mod1 <-
   nnet(
     rand.vars,
@@ -346,22 +352,22 @@ mod1 <-
     linout = T,
     trace = FALSE
   )
-#sprawdzenie działania sieci na gęstszej próbce xx1
-ypred <- predict(mod1, cbind(xx1)) #uwaga xx1 , a nie x1
+#checking the computed network on the more dense probe xx1
+ypred <- predict(mod1, cbind(xx1)) #attention! choose xx1 , not x1
 error2 <- sqrt(sum((yy1 - ypred) ^ 2))
-#powinien być ten sam błąd co dla indeksu optimise minerror
+#it should be the same minimal error as for the previously calculated in the variable minerror 
 error2
 
-#końcowy wykres
-#dane do nauki, na końcowym wykresie czerwone punkty
-#yy1 - zadana funkcja na gęstszej próbie xx1, na końcowym wykresie zielona ciągła linia
-#czarna linia to końcowa aproksymacja sieci na gęstszej próbie niż była uczona 
-#zmuszanie sieci do "wymyślania" nowych punktów, które tworzą czarną linię
+#the final picture
+#red points - original input data 
+#yy1 - the given function on the more dense probe xx1, in the final picture the green line
+#the black line is the final net approximation for the more dense probe xx1
+#
 par(mfrow = c(3, 1))
 #plot each model
 plot.nnet(mod1)
-plot(x1, y1, col = "red")       # czerwone punkty nauki funkcji sinus
-lines(xx1, yy1, col = "green")  # zielona prawdziwa funkcja sinus 
-lines(xx1, ypred)               # czarna aproksymowana przez nauczoną sieć funkcja sinus
+plot(x1, y1, col = "red")       # red points of training input data points
+lines(xx1, yy1, col = "green")  # green line as a real data from which red points were taken 
+lines(xx1, ypred)               # black line the approximated line as the trained network output
 plot(errorvector)
 
